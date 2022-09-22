361b3370cb11d7e9a3bb39012e51f2c205dd8b72
adam.metal <adam.metal@codecool.com>
2022-09-22 17:29:50 +0200

    uer deck router

--- card-api/src/user/deck/deck.router.js
+++ card-api/src/user/deck/deck.router.js
@@ -3,8 +3,22 @@ const DeckModel = require("./deck.schema");
 
 const deck = new Router();
 
+deck.use("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  if (!req.user.decks.includes(id)) {
+    return res.status(400).end();
+  }
+
+  req.deck = await DeckModel.findById(id);
+  next();
+});
+
 deck.get("/", (req, res) => {
   return res.json(req.user.decks);
+});
+
+deck.get("/:id", (req, res) => {
+  return res.json(req.deck);
 });
 
 deck.post("/", async (req, res) => {
